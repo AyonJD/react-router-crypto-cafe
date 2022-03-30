21bc181739326e8688d329dab5942ff8bf6565b0
Ayon <ayonjodder177@gmail.com>
2022-03-30 22:55:28 +0600

    Hover added in custom link

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -5,9 +5,9 @@ const CustomLink = ({ children, to, ...props }) => {
     let resolved = useResolvedPath(to);
     let match = useMatch({ path: resolved.pathname, end: true });
     return (
-        <div>
+        <div className={`${match ? "text-cyan-600" : ''}`}>
             <Link
-            style={{ color: match ? "#0891B2" : "#000" }}
+            // style={{ color: match ? "#0891B2" : "#000" }}
             to={to}
             {...props}>
             {children}
