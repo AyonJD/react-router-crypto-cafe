337918dc59babd7e24d08a4564abe77cc2b18d5d
Ayon <ayonjodder177@gmail.com>
2022-03-30 17:13:36 +0600

    Header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='flex bg-white py-6 px-5 justify-between'>
+            <h1 className='cursor-pointer text-2xl font-mono font-bold'>Crypto Caffe</h1>
+            <div className='flex gap-8 font-bold'>
+                <Link to="/">Home</Link>
+                <Link to="/coins">Coins</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
