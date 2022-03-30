94bfa7e93c325ed17dfc17b7765276a87a4df210
Ayon <ayonjodder177@gmail.com>
2022-03-30 17:18:24 +0600

    Not found page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
+import NotFound from './components/NotFound/NotFound'
 
 const App = () => {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={ <Homepage></Homepage> }></Route>
+        <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   )
---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Coins = () => {
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch()
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Coins;
